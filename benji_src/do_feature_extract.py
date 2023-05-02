9767ec66a936ec41caf6a0230e48531c6e3b8f3d
Benjamin Dayan <benjamin.l.dayan@gmail.com>
2023-05-02 12:14:42 +0200

    quick_mixin function for ordering grpahs for feature extraction

--- benji_src/do_feature_extract.py
+++ benji_src/do_feature_extract.py
@@ -6,7 +6,7 @@ import pandas as pd
 import glob
 
 import os
-os.environ['DATA_PATH'] = '/cluster/home/bdayan/girgs/FeatureExtractionOut/'
+os.environ['DATA_PATH'] = '/cluster/home/bdayan/girgs/FeatureExtractionOutLCCMini3/'
 
 
 data_dir = '/cluster/scratch/bdayan/GIRG_data/'
@@ -14,9 +14,10 @@ data_dir = '/cluster/scratch/bdayan/GIRG_data/'
 results_csv = '/cluster/home/bdayan/girgs/nemo-eva/data-paper/3-cleaned_features/results.csv'
 df = pd.read_csv(results_csv)
 
-df = df.loc[df.Model == 'real-w`orld']
+df = df.loc[df.Model == 'real-world']
 
-df = df.sort_values('Nodes')
+max_number_of_nodes = 100000
+df = df.loc[df.Nodes < max_number_of_nodes].sort_values('Nodes')
 
 
 # array([['ia-enron-only', 'ia'],
@@ -30,6 +31,28 @@ for graph_name, group in graph_name_group_pairs:
     print(fn)
     graph_dict = {"Group": group, "FullPath": fn, "Name": graph_name}
     graph_dicts.append(graph_dict)
+
+def quick_mixin(my_list, end_per_begin=5):
+    """[1,2,3,4,5,6,7,8,9,10], 3 -> [1,2,10,3,4,9,5,6,8,7] roughly"""
+    i = 0
+    j = len(my_list)-1
+    out = []
+    end_went = False
+    while i < j:
+        if i % (end_per_begin-1) == 0 and not end_went:
+            out.append(my_list[j])
+            j -= 1
+            end_went = True
+        else:
+            out.append(my_list[i])
+            i += 1
+            end_went = False
+
+    out.append(my_list[i])
+    return out
+
+
+graph_dicts = quick_mixin(graph_dicts, end_per_begin=6)
 
 # graph_name_group_pairs = df.groupby('Graph').Type.unique()
 # graph_name_group_pairs = graph_name_group_pairs.apply(lambda x: x[0])
@@ -48,10 +71,14 @@ for graph_name, group in graph_name_group_pairs:
 
 # Try first just a few
 # graph_dicts = graph_dicts[:3]
-print('running on graph dicts:')
-print(graph_dicts)
+if __name__ == '__main__':
+    print('running on graph dicts:')
+    print(graph_dicts)
 
-fe = FeatureExtractor(graph_dicts)
+    fe = FeatureExtractor(graph_dicts)
 
-print('execute fast write?')
-fe.execute_immediate_write()
+    print('execute fast write?')
+    fe.execute_immediate_write()
+
+# sbatch --time=24:00:00 --ntasks=1 --cpus-per-task=20 --mem-per-cpu=2G --wrap="python do_feature_extract.py"
+# sbatch --time=1-20 --ntasks=1 --cpus-per-task=12 --mem-per-cpu=5000 --wrap="python do_feature_extract.py"
