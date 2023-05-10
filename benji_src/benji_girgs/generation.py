Fixed up average degree for Min GIRGs and mixed GIRGs

benji_src/benji_girgs/generation.py
@@ -17,7 +17,7 @@ import os
 
 
 
-from benji_girgs.points import Points, PointsTorus, PointsCube
+from benji_girgs.points import Points, PointsTorus, PointsCube, PointsMCD, PointsTorus2, PointsTrue
 
 def torus_uniform(d: int = 2, n: int = 1000) -> np.ndarray:
     """uniformly sample from the torus"""
@@ -156,21 +156,42 @@ def edge_list_to_nk_graph(edge_list: List[Tuple[int, int]], n) -> Graph:
     return g
 
 
-def generate_GIRG_nk(n, d, tau, alpha, const=1.0, points_type=PointsTorus):
+def generate_GIRG_nk(n, d, tau, alpha, desiredAvgDegree=None, const=None, weights: Optional[np.ndarray] = None, points_type=PointsTorus):
     """Generate a GIRG of n vertices, with power law exponent tau, dimension d
     and alpha """
     # nx.from_numpy_matrix goes from an adjacency matrix. It actually
     # works fine from an upper triangular matrix (with zeros on the diagonal)
     # so all good!
-    weights = generateWeights(n, tau)
-    weights = weights / np.sqrt(np.sum(weights))
+    if weights is None:
+        weights = generateWeights(n, tau)
+
+    if const is not None and desiredAvgDegree is not None:
+        raise ValueError("Cannot specify both const and desiredAvgDegree")
+
+    if const is None:
+        if desiredAvgDegree is not None:
+            const = girgs.scaleWeights(weights, desiredAvgDegree, d, alpha)
+        else:
+            const = 1.0
+
+    # e.g. PointsTorus2 and PointsMCD are both "True Volume", whereas PointsTorus differs by a factor of 2^d.
+    # PointsTorus implementation matces CGIRGs however and hence const, so we must scale based off it.
+    const_in = const * (2 ** d) if PointsTrue in points_type.__mro__ else const
+    # This is necessary because the c from girgs.scaleWeights is actually used for w_i -> c w_i.
+    # This makes w_u w_v / W -> c w_u w_v / W, and so the true c' (w_u w_v/W / r^d)^alpha is c' = c^alpha
+    const_in = const_in ** alpha
+    print(f'const_in: {const_in}')
+
     pts = generatePositions(n, d)
     pts = points_type(pts)
-    adj_mat = generateEdges(weights, pts, alpha, const=const)
+
+    # note / np.sqrt(np.sum(weights)), s.t. w_u w_v -> (w_u / sqrt(W)) (w_v / sqrt(W)) = w_u w_v / W
+    adj_mat = generateEdges(weights / np.sqrt(np.sum(weights)), pts, alpha, const=const_in)
+    # adj_mat = generateEdges(scaled_weights, pts, alpha, const=1.0)
 
     # g = nk.nxadapter.nx2nk(nx.from_numpy_array(edges))
     g = edge_list_to_nk_graph(zip(*np.nonzero(adj_mat)), n)
-    return g, adj_mat, weights, pts
+    return g, adj_mat, weights, pts, const
 
 
 
@@ -180,7 +201,6 @@ def cgirg_gen(n, d, tau, alpha, desiredAvgDegree=None, const=None, weights: Opti
     """
     if weights is None:
         weights = girgs.generateWeights(n, tau)
-    scaled_weights = weights
 
     if const is not None and desiredAvgDegree is not None:
         raise ValueError("Cannot specify both const and desiredAvgDegree")

benji_src/benji_girgs/points.py
@@ -20,6 +20,11 @@ class Points(np.ndarray):
         Returns an (n,n) matrix of distances between points
         """
         pass
+
+# True to Volume version of Points - need adjustments in const
+class PointsTrue(Points):
+    pass
+
 class PointsTorus(Points):
     def __new__(cls, input_array):
         # Input array is an already formed ndarray instance
@@ -30,6 +35,22 @@ class PointsTorus(Points):
 
     def dists(self):
         return get_dists_julia(self)
+
+# Potentiallly we get rid of this, but its a "True to volume" version of PointsTorus - PointsTorus matches
+# C++ GIRGs.
+class PointsTorus2(PointsTrue):
+    def __new__(cls, input_array):
+        # Input array is an already formed ndarray instance
+        # We first cast to be our class type
+        obj = np.asarray(input_array).view(cls)
+        # Finally, we must return the newly created object:
+        return obj
+
+    def dists(self):
+        n, d = self.shape
+        return 2*get_dists_julia(self)
+
+
     
 class PointsCube(Points):
     def __new__(cls, input_array):
@@ -68,7 +89,7 @@ class PointsCube(Points):
 # Vol(r) = n - Prod_{i=1}^k (n^(|a_i|/d) - (2r)^|a_i|) =
 #
 # I think we'd best use this full formula throughout for consistency
-class PointsMCD(Points):
+class PointsMCD(PointsTrue):
 
     def __new__(cls, input_array):
         obj = np.asarray(input_array).view(cls)
@@ -78,7 +99,9 @@ class PointsMCD(Points):
         n, d = self.shape
         r = get_dists_mcd(self)
         # return (n**((d-1)/d) * r)**(1/d)
-        out = n - (n**(1/d) - 2*r)**d
+        # out = n - (n**(1/d) - 2*r)**d
+        out = 1 - (1 - 2*r)**d
+        # out = 1 - (1 - r)**d
         return out**(1/d)
 def get_points_simple_mixed_class(groups):
     """
@@ -86,7 +109,7 @@ def get_points_simple_mixed_class(groups):
     We get a Min(diff0, Max(diff1, diff2)) distance for absolue torus distances.
 
     """
-    class PointsSimpleMixed(Points):
+    class PointsSimpleMixed(PointsTrue):
         my_groups = groups
         def __new__(cls, input_array):
             obj = np.asarray(input_array).view(cls)
@@ -97,8 +120,8 @@ def get_points_simple_mixed_class(groups):
             r = get_dists_mixed(self, self.my_groups)
             prod = 1
             for group in self.my_groups:
-                prod *= (n**(len(group)/d) - (2*r)**len(group))
-            out = n - prod
+                prod *= (1 - (2*r)**len(group))
+            out = 1 - prod
             return out**(1/d)
 
     return PointsSimpleMixed